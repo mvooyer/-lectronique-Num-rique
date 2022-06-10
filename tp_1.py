3e69a5d618f24ac045a80eb47f1f2e2d9ccd035b
mvooyer <84793985+mvooyer@users.noreply.github.com>
2022-06-10 15:12:17 +0200

    Update tp_1.py

--- tp_1.py
+++ tp_1.py
@@ -2,7 +2,7 @@ import numpy as np
 import csv
 
 
-# Nombre d entrees
+# Nombre d entrees (permet aussi definir le nombre d entree qui seront crees et remplies dans le fichier csv
 N = 4
 # genere un tableau avec N entrees et 2**N lignes
 noms = []
@@ -25,12 +25,14 @@ def table_and(entr):
             sortie[i] = 1
     return sortie
 
+
 def table_or(entr):
     sortie = np.zeros(len(entr))
     for i in range(len(entr)):
         if sum(entr[i]) >=1:
             sortie[i] = 1
     return sortie
+
 
 def table_or_exclu(entr):
     sortie = np.zeros(len(entr))
@@ -41,20 +43,24 @@ def table_or_exclu(entr):
         sortie[i] = x
     return sortie
 
+
 # print(table_and(entrees))
 # print(table_or(entrees))
 # print(table_or_exclu(entrees))
 
 
-# ouvre/cree un fichier csv et ecrit les entrees et sorties 
-with open('sortie_4_entrees.csv', 'w', newline='') as csvfile:
+# ouvre/cree un fichier csv et ecrit les entrees et sorties
+file_name = 'sortie_' + str(N) + '_entrees.csv'
+with open(file_name, 'w', newline='') as csvfile:
     noms.append('Sortie AND \t')
     noms.append('Sortie OR \t')
     noms.append('Sortie OR_EXCLU \t')
     filewriter = csv.DictWriter(csvfile, fieldnames=noms, delimiter=';')
 
+    # cree l entete du tableau dans le csv
     filewriter.writeheader()
 
+    # calcule les differentes sorties pour le ET / OU / OU EXCLUSIF
     sortie_1 = table_and(entrees)
     sortie_2 = table_or(entrees)
     sortie_3 = table_or_exclu(entrees)
@@ -62,11 +68,14 @@ with open('sortie_4_entrees.csv', 'w', newline='') as csvfile:
         ligne = {}
         n = 0
         for e in entrees[i]:
+            # ajoute les entrees dans la ligne
             ligne[noms[n]] = str(e)
             n += 1
+        # ajoute les sorties dans le fichier
         ligne[noms[n]] = str(sortie_1[i])
         ligne[noms[n+1]] = str(sortie_2[i])
         ligne[noms[n+2]] = str(sortie_3[i])
+        # ecrit la ligne dans le fichier CSV
         filewriter.writerow(ligne)
 csvfile.close()
 
